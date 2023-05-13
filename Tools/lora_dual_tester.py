e7a6a4e8421a539ccd3b3718ee518d43c5888e1c
kgliwinski <259282@student.pwr.edu.pl>
2023-05-14 01:16:13 +0200

    something is wrong

--- Tools/lora_dual_tester.py
+++ Tools/lora_dual_tester.py
@@ -10,6 +10,8 @@ log.basicConfig(format='%(asctime)s %(message)s', level=log.INFO)
 ROSALIA_BAUD = 115200
 GS_BAUD = 19200
 
+GS_TYPE = 0
+ROSALIA_TYPE = 1
 
 def show_serial_ports() -> list:
     ports = serial.tools.list_ports.comports()
@@ -19,16 +21,32 @@ def show_serial_ports() -> list:
         prt.append(port)
     return prt
 
+def create_dataframe(sender_type : int):
+    if sender_type == GS_TYPE:
+        send_buf = bytes('GS;NOW;FRAME num.' + str(i) + '\n', 'ascii')
+    elif sender_type == ROSALIA_TYPE:
+        send_buf = bytes('ROSALIA;FRAME num.' + str(i) + '\n', 'ascii')
+    return send_buf
 
 available_ports = show_serial_ports()
 # print(available_ports)
 
 sender_num = int(input('Insert the sender serial port: '))
 receiver_num = int(input('Insert the receiver serial port: '))
+
 total_time = int(input('Insert the desired test time in seconds: '))
 
-sender_serial = serial.Serial(available_ports[sender_num], ROSALIA_BAUD)
-receiver_serial = serial.Serial(available_ports[receiver_num], GS_BAUD)
+type_int = int(input('Insert the type of sender: 0 for GS, 1 for ROSALIA: '))
+
+if type_int == GS_TYPE:
+    sender_baud = GS_BAUD
+    receiver_baud = ROSALIA_BAUD
+elif type_int == ROSALIA_TYPE:
+    sender_baud = ROSALIA_BAUD
+    receiver_baud = GS_BAUD
+
+sender_serial = serial.Serial(available_ports[sender_num], sender_baud)
+receiver_serial = serial.Serial(available_ports[receiver_num], receiver_baud)
 
 i = 0
 good_frames = 0
@@ -38,7 +56,7 @@ receive_buf = ''
 
 while_start = time.time()
 while sender_serial.is_open and receiver_serial.is_open and time.time() - while_start < total_time:
-    send_buf = bytes('FRAME num.' + str(i) + '\n', 'ascii')
+    send_buf = create_dataframe(type_int)
     sender_serial.write(send_buf)
     log.info('Writing %s', send_buf)
     time.sleep(1.)
@@ -48,7 +66,7 @@ while sender_serial.is_open and receiver_serial.is_open and time.time() - while_
             log.info('Good frame received: %s', receive_buf)
             good_frames += 1
         else:
-            log.info('Bad frame received %s:', receive_buf)
+            log.info('Bad frame received %s:', receive_buf.decode('ascii'))
             bad_frames += 1
     except:
         log.info('Frame not received')
