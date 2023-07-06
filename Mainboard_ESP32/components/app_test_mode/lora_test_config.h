4811417a2ed20c74e59588ebbb7e1660ed7d825b
kgliwinski <259282@student.pwr.edu.pl>
2023-07-06 11:40:47 +0200

    Test tasks start + kconfig

---
+++ Mainboard_ESP32/components/app_test_mode/lora_test_config.h
@@ -0,0 +1,41 @@
+// Copyright 2023 PWr in Space, Krzysztof Gliwiński
+
+#include "config.h"
+#include "driver/uart.h"
+#include "esp_console.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_vfs_dev.h"
+#include "esp_vfs_fat.h"
+#include "freertos/FreeRTOS.h"
+#include "lora_esp32_config.h"
+#include "sdkconfig.h"
+
+#define TAG "MAIN"
+
+lora_struct_t lora = {._spi_transmit = _lora_SPI_transmit,
+                      ._delay = _lora_delay,
+                      ._gpio_set_level = _lora_GPIO_set_level,
+                      .log = _lora_log,
+                      .rst_gpio_num = CONFIG_RS_LORA,
+                      .cs_gpio_num = CONFIG_CS_LORA,
+                      .d0_gpio_num = CONFIG_D0_LORA,
+                      .implicit_header = 0,
+                      .frequency = 0};
+
+/*!
+ * \brief Initiates lora and uart
+*/
+void lora_test_init();
+
+/*!
+ * \brief LoRa sender task - sends data from UART to LoRa
+ * \param[in] param Unused - FreeRTOS task parameter
+*/
+void task_lora_tx(void *param);
+
+/*!
+ * \brief LoRa receiver task - receives data from LoRa and sends it to UART
+ * \param[in] param Unused - FreeRTOS task parameter
+*/
+void task_lora_rx(void *param);
--- Mainboard_ESP32/main/main.c
+++ Mainboard_ESP32/main/main.c
@@ -3,78 +3,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "config.h"
-#include "driver/uart.h"
-#include "esp_console.h"
-#include "esp_log.h"
-#include "esp_system.h"
-#include "esp_vfs_dev.h"
-#include "esp_vfs_fat.h"
 #include "freertos/FreeRTOS.h"
-#include "lora_esp32_config.h"
-
+#include "esp_log.h"
+#include "freertos/task.h"
 #define TAG "MAIN"
 
-lora_struct_t lora = {._spi_transmit = _lora_SPI_transmit,
-                      ._delay = _lora_delay,
-                      ._gpio_set_level = _lora_GPIO_set_level,
-                      .log = _lora_log,
-                      .rst_gpio_num = RS_LORA,
-                      .cs_gpio_num = CS_LORA,
-                      .d0_gpio_num = D0_LORA,
-                      .implicit_header = 0,
-                      .frequency = 0};
-
-// TODO(Glibong): Save this as a task called by CLI (when fully finished)
-void task_tx(void *p) {
-  char buf[32];
-  // size_t len = 0;
-  // ssize_t lineSize = 0;
-  for (;;) {
-    fgets(buf, 32, stdin);
-    // strncat(buf, '\n', 1);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    ESP_LOGI(TAG, "sending packet: %s\n", buf);
-    lora_send_packet(&lora, (uint8_t *)buf, strlen(buf));
-    ESP_LOGI(TAG, "packet sent...\n");
-  }
-}
-
-void task_rx(void *p) {
-  int x;
-  uint8_t buf[32];
-  for (;;) {
-    lora_set_receive_mode(&lora);  // put into receive mode
-    while (lora_received(&lora) == LORA_OK) {
-      x = lora_receive_packet(&lora, buf, sizeof(buf));
-      buf[x] = 0;
-      printf("Received: %s\n", buf);
-      lora_received(&lora);
-    }
-    vTaskDelay(10);
-  }
-}
-
 void app_main(void) {
-  _lora_spi_and_pins_init();
-  lora_init(&lora);
-
-  vTaskDelay(pdMS_TO_TICKS(100));
-
-  lora_set_frequency(&lora, 867e6);
-  lora_set_bandwidth(&lora, LORA_BW_250_kHz);
-  lora_disable_crc(&lora);
-
-  int16_t read_val_one = lora_read_reg(&lora, 0x0d);
-  int16_t read_val_two = lora_read_reg(&lora, 0x0c);
-  ESP_LOGI(TAG, "LORA_READ: %04x, %04x", read_val_one, read_val_two);
-
-  ESP_ERROR_CHECK(
-      uart_driver_install(CONFIG_ESP_CONSOLE_UART_NUM, 256, 0, 0, NULL, 0));
-  esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
-  esp_vfs_dev_uart_port_set_rx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
-                                            ESP_LINE_ENDINGS_CR);
-  esp_vfs_dev_uart_port_set_tx_line_endings(CONFIG_ESP_CONSOLE_UART_NUM,
-                                            ESP_LINE_ENDINGS_CRLF);
-  xTaskCreate(&task_tx, "task_tx", 2048, NULL, 5, NULL);
+  ESP_LOGI(TAG, "Hello world!");
+  vTaskDelay(pdMS_TO_TICKS(1000));
+  ESP_LOGI(TAG, "Goodbye world!");
+  vTaskDelete(NULL);
 }
