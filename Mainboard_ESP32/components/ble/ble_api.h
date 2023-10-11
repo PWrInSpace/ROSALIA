a2bb44c3e63f2f61bb06b2ffb5c3ea5955ceaf48
kgliwinski <259282@student.pwr.edu.pl>
2023-10-11 19:25:11 +0200

    Some cleanup done

--- Mainboard_ESP32/components/ble/ble_api.h
+++ Mainboard_ESP32/components/ble/ble_api.h
@@ -8,14 +8,59 @@
 #include "esp_bt.h"
 #include "esp_bt_defs.h"
 #include "esp_bt_main.h"
-#include "sdkconfig.h"
 #include "nvs_flash.h"
+#include "sdkconfig.h"
 
 /*!
  * \file ble_api.h
  * \brief BLE API - contains all BLE functions and structures
  * ass well as attributes of GATT - unified in one structure
  */
+
+/* LSB
+ <-------------------------------------------------------------------------------->
+ MSB */
+// first uuid, 16bit, [12],[13] is the value
+// second uuid, 32bit, [12], [13], [14], [15] is the value
+#define BLE_UUID_CONFIG_DEFAULT()                                           \
+  {                                                                         \
+    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, \
+        0xEE, 0x00, 0x00, 0x00, 0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00,   \
+        0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,               \
+  }
+
+// slave connection min interval, Time = min_interval * 1.25 msec
+// slave connection max interval, Time = max_interval * 1.25 msec
+// TEST_MANUFACTURER_DATA_LEN,
+// &test_manufacturer[0],
+#define BLE_ADV_DATA_CONFIG_DEFAULT()                                          \
+  {                                                                            \
+    .set_scan_rsp = false, .include_name = true, .include_txpower = false,     \
+    .min_interval = 0x0006, .max_interval = 0x0010, .appearance = 0x00,        \
+    .manufacturer_len = 0, .p_manufacturer_data = NULL, .service_data_len = 0, \
+    .p_service_data = NULL, .service_uuid_len = sizeof(adv_service_uuid128),   \
+    .p_service_uuid = adv_service_uuid128,                                     \
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),      \
+  }
+
+// TEST_MANUFACTURER_DATA_LEN,
+// &test_manufacturer[0],
+#define BLE_SCAN_RSP_DATA_CONFIG_DEFAULT()                                  \
+  {                                                                         \
+    .set_scan_rsp = true, .include_name = true, .include_txpower = true,    \
+    .appearance = 0x00, .manufacturer_len = 0, .p_manufacturer_data = NULL, \
+    .service_data_len = 0, .p_service_data = NULL,                          \
+    .service_uuid_len = sizeof(adv_service_uuid128),                        \
+    .p_service_uuid = adv_service_uuid128,                                  \
+    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),   \
+  }
+
+#define BLE_ADV_PARAMS_CONFIG_DEFAULT()                                 \
+  {                                                                     \
+    .adv_int_min = 0x20, .adv_int_max = 0x40, .adv_type = ADV_TYPE_IND, \
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC, .channel_map = ADV_CHNL_ALL, \
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,             \
+  }
 
 /*!
  * \brief BLE status enum
