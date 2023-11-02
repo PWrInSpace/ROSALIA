51fb8f8aaa5ef1c1ed9a48d08be812fd02c68c75
kgliwinski <259282@student.pwr.edu.pl>
2023-11-02 19:59:18 +0100

    Lint

--- Mainboard_ESP32/components/mcu_config/lora_esp32_config.c
+++ Mainboard_ESP32/components/mcu_config/lora_esp32_config.c
@@ -59,7 +59,8 @@ esp_err_t _lora_spi_and_pins_init(gpio_num_t _rst_gpio_num,
   };
   ret |= gpio_config(&io_conf2);
 
-  //TODO(Glibus): check if interrupt is initiated correctly, implement interrupt
+  // TODO(Glibus): check if interrupt is initiated correctly, implement
+  // interrupt
   gpio_config_t io_conf3 = {
       .intr_type = GPIO_INTR_LOW_LEVEL,
       .mode = GPIO_MODE_INPUT,
