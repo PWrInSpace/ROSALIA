7851d2ebdefad890ce18ec7edaa9747968827f16
kgliwinski <259282@student.pwr.edu.pl>
2023-11-11 17:04:04 +0100

    Written USB MSC api, not tested yet

--- Mainboard_ESP32/components/usb_msc/usb_msc_api.c
+++ Mainboard_ESP32/components/usb_msc/usb_msc_api.c
@@ -3,3 +3,51 @@
 #include "usb_msc_api.h"
 
 #define USB_API_TAG "USB_MSC_API"
+
+esp_err_t usb_msc_init(usb_msc_config_t* config) {
+  esp_err_t ret = ESP_OK;
+  ret |= tinyusb_msc_storage_init_sdmmc(&config->sd_config);
+  if (ret != ESP_OK) {
+    ESP_LOGE(USB_API_TAG, "MSC Storage sdmmc init failed");
+    return ret;
+  }
+  ret |= usb_msc_mount(config);
+  if (ret != ESP_OK) {
+    ESP_LOGE(USB_API_TAG, "MSC Storage mount failed");
+    return ret;
+  }
+  ESP_LOGI(USB_API_TAG, "MSC initialization");
+  ret |= tinyusb_driver_install(&config->tusb_cfg);
+  return ret;
+}
+
+void usb_msc_deinit(usb_msc_config_t* config) {
+  ESP_LOGI(USB_API_TAG, "MSC storage deinitialization");
+  tinyusb_msc_storage_deinit();
+}
+
+esp_err_t usb_msc_mount(usb_msc_config_t* config) {
+  esp_err_t ret = ESP_OK;
+  ret |= tinyusb_msc_storage_mount(config->mount_path);
+  if (ret != ESP_OK) {
+    ESP_LOGE(USB_API_TAG, "MSC Storage mount failed");
+    return ret;
+  }
+  ESP_LOGI(USB_API_TAG, "MSC Storage mounted");
+  return ret;
+}
+
+esp_err_t usb_msc_unmount() {
+  esp_err_t ret = ESP_OK;
+  ret |= tinyusb_msc_storage_unmount();
+  if (ret != ESP_OK) {
+    ESP_LOGE(USB_API_TAG, "MSC Storage unmount failed");
+    return ret;
+  }
+  ESP_LOGI(USB_API_TAG, "MSC Storage unmounted");
+  return ret;
+}
+
+bool usb_msc_check_storage_use_by_host() {
+  return tinyusb_msc_storage_in_use_by_usb_host();
+}
--- Mainboard_ESP32/components/usb_msc/usb_msc_api.h
+++ Mainboard_ESP32/components/usb_msc/usb_msc_api.h
@@ -9,7 +9,10 @@
 
 #include <dirent.h>
 #include <errno.h>
+#include <stdbool.h>
 
+#include "esp_err.h"
+#include "esp_log.h"
 #include "tinyusb.h"
 #include "tusb_msc_storage.h"
 
@@ -22,6 +25,8 @@ typedef struct {
   tusb_desc_device_t desc_device;
   tusb_desc_configuration_t desc_cfg;
   tinyusb_config_t tusb_cfg;
+  tinyusb_msc_sdmmc_config_t sd_config;
+  const char* mount_path;
 } usb_msc_config_t;
 
 /*!
@@ -29,3 +34,27 @@ typedef struct {
  * \param config USB MSC configuration
  */
 esp_err_t usb_msc_init(usb_msc_config_t* config);
+
+/*!
+ * \brief USB MSC device deinitialization
+ * \param config USB MSC configuration
+ */
+void usb_msc_deinit(usb_msc_config_t* config);
+
+/*!
+ * \brief Mount USB MSC storage
+ * \param config USB MSC configuration
+ */
+esp_err_t usb_msc_mount(usb_msc_config_t* config);
+
+/*!
+ * \brief Unmount USB MSC storage
+ * \param config USB MSC configuration
+ */
+esp_err_t usb_msc_unmount();
+
+/*!
+ * \brief Check if USB MSC storage is in use by host
+ * \returns true if USB MSC storage is in use by host
+ */
+bool usb_msc_check_storage_use_by_host();
