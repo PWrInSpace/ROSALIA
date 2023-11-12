5999800c859692415bbd8e187fbf0740e0f29fbe
kgliwinski <259282@student.pwr.edu.pl>
2023-11-12 16:57:36 +0100

    Added spi flash option on usb msc

--- Mainboard_ESP32/components/usb_msc/usb_msc_api.c
+++ Mainboard_ESP32/components/usb_msc/usb_msc_api.c
@@ -4,13 +4,35 @@
 
 #define USB_API_TAG "USB_MSC_API"
 
-esp_err_t usb_msc_init(usb_msc_config_t* config) {
+esp_err_t usb_msc_init(usb_msc_config_t* config,
+                       usb_msc_init_storage_type_t storage_type) {
   esp_err_t ret = ESP_OK;
-  ret |= tinyusb_msc_storage_init_sdmmc(&config->sd_config);
-  if (ret != ESP_OK) {
-    ESP_LOGE(USB_API_TAG, "MSC Storage sdmmc init failed");
-    return ret;
+  switch (storage_type) {
+    case USB_MSC_INIT_STORAGE_TYPE_SDMMC:
+      ret |= tinyusb_msc_storage_init_sdmmc(&config->sd_config);
+      if (ret != ESP_OK) {
+        ESP_LOGE(USB_API_TAG, "MSC Storage sdmmc init failed");
+        return ret;
+      }
+      break;
+    case USB_MSC_INIT_STORAGE_TYPE_SPI_FLASH:
+      ret |= usb_msc_storage_init_spiflash(config);
+      if (ret != ESP_OK) {
+        ESP_LOGE(USB_API_TAG,
+                 "MSC Storage spi flash init failed (wear levelling)");
+        return ret;
+      }
+      ret |= tinyusb_msc_storage_init_spiflash(&config->spiflash_config);
+      if (ret != ESP_OK) {
+        ESP_LOGE(USB_API_TAG, "MSC Storage spi flash init failed");
+        return ret;
+      }
+      break;
+    default:
+      ESP_LOGE(USB_API_TAG, "MSC Storage init failed - wrong storage type");
+      return ESP_ERR_INVALID_ARG;
   }
+  config->current_storage_type = storage_type;
   ret |= usb_msc_mount(config);
   if (ret != ESP_OK) {
     ESP_LOGE(USB_API_TAG, "MSC Storage mount failed");
@@ -23,6 +45,9 @@ esp_err_t usb_msc_init(usb_msc_config_t* config) {
 
 void usb_msc_deinit(usb_msc_config_t* config) {
   ESP_LOGI(USB_API_TAG, "MSC storage deinitialization");
+  if (config->current_storage_type == USB_MSC_INIT_STORAGE_TYPE_SPI_FLASH) {
+    usb_msc_storage_deinit_spiflash(config);
+  }
   tinyusb_msc_storage_deinit();
 }
 
@@ -50,4 +75,23 @@ esp_err_t usb_msc_unmount() {
 
 bool usb_msc_check_storage_use_by_host() {
   return tinyusb_msc_storage_in_use_by_usb_host();
+}
+
+esp_err_t usb_msc_storage_init_spiflash(usb_msc_config_t* config) {
+  ESP_LOGI(USB_API_TAG, "Initializing wear levelling");
+
+  const esp_partition_t* data_partition = esp_partition_find_first(
+      ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_FAT, NULL);
+  if (data_partition == NULL) {
+    ESP_LOGE(USB_API_TAG,
+             "Failed to find FATFS partition. Check the partition table.");
+    return ESP_ERR_NOT_FOUND;
+  }
+
+  return wl_mount(data_partition, &config->wl_handle);
+}
+
+esp_err_t usb_msc_storage_deinit_spiflash(usb_msc_config_t* config) {
+  ESP_LOGI(USB_API_TAG, "Deinitializing wear levelling");
+  return wl_unmount(config->wl_handle);
 }
--- Mainboard_ESP32/components/usb_msc/usb_msc_api.h
+++ Mainboard_ESP32/components/usb_msc/usb_msc_api.h
@@ -16,6 +16,12 @@
 #include "tinyusb.h"
 #include "tusb_msc_storage.h"
 
+typedef enum {
+  USB_MSC_INIT_STORAGE_TYPE_SDMMC = 0x01,
+  USB_MSC_INIT_STORAGE_TYPE_SPI_FLASH = 0x10,
+  USB_MSC_INIT_STORAGE_NONE = 0x00,
+} usb_msc_init_storage_type_t;
+
 /*!
  * \brief USB MSC configuration struct
  * \param desc_device USB device descriptor
@@ -26,14 +32,18 @@ typedef struct {
   tusb_desc_configuration_t desc_cfg;
   tinyusb_config_t tusb_cfg;
   tinyusb_msc_sdmmc_config_t sd_config;
+  tinyusb_msc_spiflash_config_t spiflash_config;
+  wl_handle_t wl_handle;
   const char* mount_path;
+  usb_msc_init_storage_type_t current_storage_type;
 } usb_msc_config_t;
 
 /*!
  * \brief USB MSC device initialization
  * \param config USB MSC configuration
  */
-esp_err_t usb_msc_init(usb_msc_config_t* config);
+esp_err_t usb_msc_init(usb_msc_config_t* config,
+                       usb_msc_init_storage_type_t storage_type);
 
 /*!
  * \brief USB MSC device deinitialization
@@ -58,3 +68,15 @@ esp_err_t usb_msc_unmount();
  * \returns true if USB MSC storage is in use by host
  */
 bool usb_msc_check_storage_use_by_host();
+
+/*!
+ * \brief Initialize USB MSC storage on SPI flash
+ * \param config USB MSC configuration
+ */
+esp_err_t usb_msc_storage_init_spiflash(usb_msc_config_t* config);
+
+/*!
+ * \brief Deinitialize USB MSC storage on SPI flash
+ * \param config USB MSC configuration
+ */
+esp_err_t usb_msc_storage_deinit_spiflash(usb_msc_config_t* config);
